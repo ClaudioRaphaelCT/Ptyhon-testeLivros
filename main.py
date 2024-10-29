f60c37b84bef1ded2ae1028a0a4dcd52084b67a1
tsbcteixeira <167436439+tsbcteixeira@users.noreply.github.com>
2024-10-29 12:03:28 -0300

    secondd

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from views.livros import api_router
+
 app = FastAPI()
+
+# Configuração de CORS
+origins = [
+    "http://localhost:5173",  # Adicione aqui o domínio do seu frontend
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Domínios permitidos
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos os métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permite todos os cabeçalhos
+)
 
 app.include_router(api_router)
 
--- views/livros.py
+++ views/livros.py
@@ -1,33 +1,4 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from models.livros import Livro
-from controllers.livros import get_all
-from typing import List
-from fastapi import APIRouter
+from db.livros import livros_db
 
-# Criação da aplicação FastAPI
-app = FastAPI()
-
-# Configuração do CORS
-origins = [
-    "http://localhost:5173",  # Permite acesso do seu frontend (Vue.js)
-    "https://ptyhon-testelivros.onrender.com",  # Permite acesso da API em produção, se necessário
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],  # Permite todos os métodos (GET, POST, etc.)
-    allow_headers=["*"],  # Permite todos os cabeçalhos
-)
-
-# Criação do APIRouter
-api_router = APIRouter()
-
-@api_router.get("/livros", response_model=List[Livro])
-def read_livros():
-    return get_all()
-
-# Inclui o router na aplicação
-app.include_router(api_router)
+def get_all():
+    return livros_db
